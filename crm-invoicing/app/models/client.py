a477a5711824c6aec0d59e7e781fcf04b86a5a95
radomirbrkovic <radomir.brkovic@pmf.edu.rs>
2025-12-27 20:43:54 +0100

    CRM Invoicing - Create clients model and migrations

--- crm-invoicing/app/models/client.py
+++ crm-invoicing/app/models/client.py
@@ -1,9 +1,9 @@
 from sqlalchemy import String, Column, Enum
 from sqlalchemy.orm import Mapped, mapped_column
-from fastkit_core.database import BaseWithTimestamps, SoftDeleteMixin, TranslatableMixin, IntIdMixin
+from fastkit_core.database import BaseWithTimestamps, SoftDeleteMixin, IntIdMixin
 from app.models.enums import Languages
 
-class Client(IntIdMixin, BaseWithTimestamps, SoftDeleteMixin, TranslatableMixin):
+class Client(IntIdMixin, BaseWithTimestamps, SoftDeleteMixin):
     __tablename__ = "clients"
 
     name: Mapped[str] = mapped_column(String(255))
