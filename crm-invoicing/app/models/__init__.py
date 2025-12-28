46e257f9fc4d70d7818dd6980be4faf371615fd2
radomirbrkovic <radomir.brkovic@pmf.edu.rs>
2025-12-28 15:17:29 +0100

    CRM Invoicing - Create products migration

--- crm-invoicing/app/models/__init__.py
+++ crm-invoicing/app/models/__init__.py
@@ -1,9 +1,11 @@
 from fastkit_core.database import Base
 from app.models.user import User
 from app.models.client import Client
+from app.models.product import Product
 
 __all__ = [
     'Base',
     'User',
-    'Client'
+    'Client',
+    'Product'
 ]
