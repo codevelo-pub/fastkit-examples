14b97c1e1d2b4bd0ba08a565fe96a992e27e0f1a
radomirbrkovic <radomir.brkovic@pmf.edu.rs>
2025-12-29 16:37:30 +0100

    CRM Invoicing - Services

---
+++ crm-invoicing/app/services/__init__.py
@@ -0,0 +1,11 @@
+from app.service.cleint_service import ClientService
+from app.service.product_service import ProductService
+from app.service.invoice_service import InvoiceService
+from app.service.invoice_item_service import InvoiceItemService
+
+__all__ = [
+    'ClientService',
+    'ProductService',
+    'InvoiceService',
+    'InvoiceItemService',
+]
