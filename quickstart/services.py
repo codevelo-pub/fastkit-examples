fe5204deb08c511ec0f24902e7c6d8aa4f7dc19f
radomirbrkovic <radomir.brkovic@pmf.edu.rs>
2026-01-13 17:53:32 +0100

    Quick start - update service

--- quickstart/services.py
+++ quickstart/services.py
@@ -1,15 +1,15 @@
 from fastkit_core.services import BaseCrudService
 from fastkit_core.database import Repository
-from models import Todo, TodoCreate, TodoUpdate
+from models import Todo, TodoCreate, TodoUpdate, TodoResponse
 from sqlalchemy.orm import Session
 
 
-class TodoService(BaseCrudService[Todo, TodoCreate, TodoUpdate]):
+class TodoService(BaseCrudService[Todo, TodoCreate, TodoUpdate, TodoResponse]):
     """Service for Todo business logic."""
 
     def __init__(self, session: Session):
         repository = Repository(Todo, session)
-        super().__init__(repository)
+        super().__init__(repository,  response_schema=TodoResponse)
 
     def mark_completed(self, todo_id: int) -> Todo:
         """Mark a todo as completed."""
