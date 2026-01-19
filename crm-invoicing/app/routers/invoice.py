0ae520813d86b65291f922a6e972b1cc952493a4
radomirbrkovic <radomir.brkovic@pmf.edu.rs>
2026-01-19 19:21:30 +0100

    CRM Invoices - Load relations in list of invoices

--- crm-invoicing/app/routers/invoice.py
+++ crm-invoicing/app/routers/invoice.py
@@ -3,10 +3,11 @@ from app.infrastructure.auth import current_active_user
 from fastkit_core.http import success_response, paginated_response
 from fastkit_core.database import get_async_db
 from fastkit_core.i18n import _
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, selectinload
 from starlette.responses import JSONResponse
 from app.schemas import InvoiceCreate, InvoiceUpdate
 from app.services import InvoiceService
+from app.models import Invoice, InvoiceItem
 
 router = APIRouter(
     prefix='/invoices',
@@ -19,7 +20,11 @@ def get_service(session: Session = Depends(get_async_db)) -> InvoiceService:
 
 @router.get('', name='api.invoices.index')
 async def index(page: int = 1, per_page: int = 10, service: InvoiceService = Depends(get_service)) -> JSONResponse:
-    invoices, meta = await service.paginate(page= page, per_page=per_page)
+    invoices, meta = await service.paginate(
+        page= page,
+        per_page=per_page,
+        _load_relations=[selectinload(Invoice.items).selectinload(InvoiceItem.product)]
+    )
     return paginated_response(items=[invoice.model_dump() for invoice in invoices], pagination=meta)
 
 @router.post('', name='api.invoices.store')
