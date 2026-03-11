d16fabfb9115aaf36b4f422690ce7c7d7e87ae62
radomirbrkovic <radomir.brkovic@pmf.edu.rs>
2026-03-11 21:13:55 +0100

    Move to module - Invoice Item schemas

--- crm-invoicing/app/schemas/__init__.py
+++
@@ -1,9 +0,0 @@
-from app.schemas.invoice_item import (
-    InvoiceItemCreate,
-    InvoiceItemUpdate,
-    InvoiceItemResponse
-)
-
-__all__ = [
-    'InvoiceItemCreate', 'InvoiceItemUpdate', 'InvoiceItemResponse'
-]
