80b397014f68e2414bea26cb636aa6ba7250ddb4
radomirbrkovic <radomir.brkovic@pmf.edu.rs>
2026-04-18 12:45:44 +0200

    Create Product module

--- fastkit-invoicing/migrations/env.py
+++ fastkit-invoicing/migrations/env.py
@@ -23,6 +23,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 from modules.clientses.models import Clients  # noqa
+from modules.products.models import Product  # noqa
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
